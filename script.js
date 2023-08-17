f49f3dfdd8e6615cf67352eb583771035a0bcbc6
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-08-17 10:14:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function highlight() {
-    //Write your code here
-
-
+    document.querySelectorAll("strong").forEach(e => e.style.color ='green')
 }
 
 
 function return_normal() {
     //Write your code here
-
+document.querySelectorAll("strong").forEach(e => e.style.color ='')
     
 }
